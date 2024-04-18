51e49d1153f2b13b1a840790a4056f6b52ddb3e4
Michael Whitaker <mcwhitaker@fastmail.com>
2024-04-18 14:59:24 -0500

    feat: Update devbox to use flake for updating ksops install

---
+++ overlays/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A flake that applies fixes to packages we need for the project"
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlay = (final: prev: {
+        # kustomize-sops in nixpkgs uses the old location and install format
+        # this fixes the install path until the pkg gets updated properly
+        # https://github.com/NixOS/nixpkgs/issues/175515
+        kustomize-sops = prev.kustomize-sops.overrideAttrs {
+          installPhase = ''
+            install -Dm644 "$GOPATH/bin/kustomize-sops" "$out/bin/ksops"
+          '';
+        };
+      });
+
+      pkgs =
+        import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+
+    in rec {
+      packages = {
+        kustomize-sops = pkgs.kustomize-sops;
+      };
+    }
+  );
+}
